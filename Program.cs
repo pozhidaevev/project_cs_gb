3d3cec57e4ed160e3758406451d2c03b7b7f80a7
evgeny <111169883+pozhidaevev@users.noreply.github.com>
2023-08-20 15:48:00 +0500

    Добавление кода решения задачи
    Добавление кода решения задачи

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+string[] array = {"Hello", "2", "world", ":-)"};
+
+string[] newArray = new string[array.Length];
+
+void CreateNewArray(string[] array, string[] newArray)
+{
+    int i = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+    if(array[j].Length <= 3)
+        {
+        newArray[i] = array[j];
+        i++;
+        }
+    }
+}
+
+void PrintNewArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+CreateNewArray(array, newArray);
+PrintNewArray(newArray);
